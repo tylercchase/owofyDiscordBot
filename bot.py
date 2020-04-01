5a70d1ebd372795b558edef85100c46cdd23c9c2
tylercchase <tylercchase@gmail.com>
2020-04-01 04:29:03 -0800

    Owoify name too

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
-# Work with Python 3.6
 import discord
-from filter import process_image
+from filter import process_image, owoify_text
+from owotrans import owo
 TOKEN = ''
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
+
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('owo'):
+        # owoified_name = owoify_text(message.author.nick)
+        # try: 
+        #     if len(owoified_name) <= 32:
+        #         await message.author.edit(nick=owoified_name, reason="OwO")
+        # except:
+        #     print("unable to do nick change")
         img = process_image(message.author.avatar_url)
         fileA = discord.File(img,filename="hello.png")
         await message.channel.send(file=fileA)
--- filter.py
+++ filter.py
@@ -4,18 +4,48 @@ from urllib.request import urlopen, Request
 import io
 
 def process_image(avatar_url,readFlag=cv2.IMREAD_COLOR):
-    # req = Request('https://cdn.discordapp.com/avatars/163352834572681216/e17aca9ec772c87ab12918b203a16c7f.webp', headers = {"User-Agent": "Mozilla/5.0"})
-    # resp = urlopen(req)
-    resp = urlopen('http://hotdog.tylerchase.sexy/static/img/e865059.png')
+    #get and download image to memory
+    req = Request(avatar_url, headers = {"User-Agent": "Mozilla/5.0"})
+    resp = urlopen(req)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, readFlag)
-    response,buffer = cv2.imencode(".jpg",image)
-    # decode_img = cv2.imdecode(np.frombuffer(io_buff.getbuffer(), np.uint8),-1)
-    # img = np.ones((100, 100), np.uint8)
-
-    # is_success, buffer = cv2.imencode(".jpg", img)
-    io_buf = io.BytesIO(buffer)
-    # decode
-    # decode_img = cv2.imdecode(np.frombuffer(io_buf.getbuffer(), np.uint8), -1)
     
+
+    #Add pink overlay
+    overlay = image.copy()
+    cv2.rectangle(overlay,(0,0),(200,200),(221, 50, 230),-1)
+    opacity = 0.5
+    cv2.addWeighted(overlay,opacity,image, 1 - opacity, 0, image)
+
+    petals = cv2.imread("petals.png")
+    petals = cv2.resize(petals, (110,110))
+    final = cv2.addWeighted(image,1,petals,0.5,1)
+    
+    buffer = cv2.imencode(".png",final)
+
+    io_buf = io.BytesIO(buffer)
+
     return io_buf
+
+def owoify_text(text):
+    smileys = [';;w;;', '^w^', '>w<', 'UwU', '(・`ω´・)', '(´・ω・`)']
+    words = text.split()
+    first_letter = words[0][0]
+    letter_stutter = f"{first_letter}-{first_letter.lower()}-{first_letter.lower()}"
+    if len(words[0]) > 1:
+        words[0] = letter_stutter + words[0][1:]
+    else:
+        words[0] = letter_stutter
+    text = " ".join(words)
+    text = text.replace('L', 'W').replace('l', 'w')
+    text = text.replace('R', 'W').replace('r', 'w')
+    text = '! {}'.format(choice(smileys)).join(text.rsplit('!', 1))
+    text = '? OwO'.join(text.rsplit('?', 1))
+    text = '. {}'.format(choice(smileys)).join(text.rsplit('.', 1))
+    text = f"{text} desu"
+    for v in ['a', 'o', 'u', 'A', 'O', 'U']:
+        if 'n{}'.format(v) in text:
+            text = text.replace('n{}'.format(v), 'ny{}'.format(v))
+        if 'N{}'.format(v) in text:
+            text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
+    return text
